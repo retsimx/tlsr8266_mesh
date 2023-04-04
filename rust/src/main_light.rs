ba0fd44f12b3b21009e2081180c8d4619bd55dd8
Lewis Lakerink <llakerink@swin.edu.au>
2023-04-04 18:55:53 +1000

    Don't reboot after clearing lum state

--- rust/src/main_light.rs
+++ rust/src/main_light.rs
@@ -433,7 +433,6 @@ pub extern "C" fn rf_link_data_callback(p: *const ll_packet_l2cap_data_t) {
     } else if op == LGT_CLEAR_LUM_STATE {
         // Clear the lum state
         flash_erase_sector(*get_flash_adr_lum());
-        _light_sw_reboot();
     } else if op == LGT_CMD_KICK_OUT {
         irq_disable();
         kick_out((params[0] as u32).try_into().unwrap());
--- rust/src/version.rs
+++ rust/src/version.rs
@@ -1,1 +1,1 @@
-pub static BUILD_VERSION: u32 = 173;
+pub static BUILD_VERSION: u32 = 175;

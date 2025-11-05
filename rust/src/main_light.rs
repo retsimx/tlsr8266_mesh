7f515c11d67cfe1a584346f7c2a08e6b41704506
Lewis Lakerink <llakerink@swin.edu.au>
2025-11-05 19:01:51 +1100

    Print MAC address using correct order

--- rust/src/main_light.rs
+++ rust/src/main_light.rs
@@ -122,11 +122,11 @@ pub fn user_init() {
 
     rf_link_slave_init(40000);
 
-    // Print MAC address now that it has been loaded from flash
+    // Print MAC address now that it has been loaded from flash (reversed byte order)
     let mac = MAC_ID.lock();
     uprintln!(
         "MAC Address: {:02X}:{:02X}:{:02X}:{:02X}:{:02X}:{:02X}",
-        mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]
+        mac[5], mac[4], mac[3], mac[2], mac[1], mac[0]
     );
 
     factory_reset_handle();
